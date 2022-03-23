c9a6fbf00505b53f89931609b3973ebc7752eaa7
Anvar <63100534+anvar099@users.noreply.github.com>
2022-03-23 22:37:51 +0500

    home page responsive done

---
+++ Home Page/Home.js
@@ -0,0 +1,20 @@
+// var indexValue = 2;
+// showCard(indexValue);
+// function side_slide(e) {
+//   showCard(indexValue += e);
+// }
+// function showCard(e) {
+//   var i;
+//   const card = document.querySelectorAll(".hello");
+//   if(e > card.length) {
+//     indexValue = 2;
+//   }
+//   if(e < 2) {
+//     indexValue = card.length
+//   }
+//   for(i = 0; i < card.length; i++) {
+//     card[i].style.display = "none";
+//   }
+//   card[indexValue - 1].style.display = "block";
+// }
+
